038cf3bc2522945dbb3057b08194a8a1061cf063
Gordon Evans <gjrevans@gmail.com>
2017-03-05 23:10:42 -0500

    added index page and time stamp service

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var server = express();
+
+var port = process.env.PORT || 8000;
+
+// Listen on the provided port
+server.listen(port, function () {
+  console.log('Listening on port ' + port)
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// Serve a static index page if no params are provided
+server.get('/', function(req, res) {
+    var fileName = path.join(__dirname, 'index.html');
+    res.sendFile(fileName, function(err){
+        if(err) {
+            res.writeHead(err.status);
+            res.end();
+        }
+    });
+});
+
+server.get('/:date', function (req, res) {
+    var date = req.params.date;
+
+    // Try to parse the date as a natural data value
+    var newDate = moment(date, "MMMM DD, YYYY");
+
+    // Then if date is invalid, try to parse as unix
+    if(!newDate.isValid()){
+        newDate = moment.unix(date);
+    }
+
+    // If the date is now valid, send it back, otherwise return null object
+    if(newDate.isValid()){
+        res.json({
+            unix: newDate.format("X"),
+            natural: newDate.format("MMMM D, YYYY")
+        });
+    }
+
+    res.json({
+        unix: null,
+        natural: null
+    });
+});
